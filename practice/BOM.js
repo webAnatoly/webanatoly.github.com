0b809d9a5e8d10b35533d85d44046c5f16e1d5bd
Anatoly <webAnatoly@users.noreply.github.com>
2017-01-18 15:03:09 +0300

    Add files via upload

---
+++ practice/BOM.js
@@ -0,0 +1,37 @@
+; 'use strict';
+// Detecting The Browser
+
+var browsers = {
+
+	// Opera 8.0+
+	isOpera: (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0,
+
+	// Firefox 1.0+
+	isFirefox: typeof InstallTrigger !== 'undefined',
+
+	// Safari 3.0+ "[object HTMLElementConstructor]" 
+	isSafari: Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0 || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification),
+
+	// Internet Explorer 6-11
+	isIE: /*@cc_on!@*/false || !!document.documentMode,
+
+	// Edge 20+
+	isEdge: !this.isIE && !!window.StyleMedia,
+
+	// Chrome 1+
+	isChrome: !!window.chrome && !!window.chrome.webstore,
+
+	// Blink engine detection
+	isBlink: (this.isChrome || this.isOpera) && !!window.CSS
+
+};
+
+for (var key in browsers) {
+	console.log(key + ": " + browsers[key]);
+	if (browsers[key] == true) {
+		var detectedBrowser = key;
+	}
+}
+
+document.getElementById('yourBrowser').innerHTML = 'Your browser is ' + detectedBrowser.substring(2);
+
