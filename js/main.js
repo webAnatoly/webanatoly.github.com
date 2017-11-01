237edd8d2ccee6aa786d36c33c90ba089695f43d
tolik.demianov <tolik.demianov@gmail.com>
2017-11-01 10:49:14 +0300

    change main html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-let canv = document.getElementById('myCanvas');
-let context = canv.getContext("2d");
-context.font = "30px Arial";
-context.fillText("Hello I'm a Canvas",20,60);
+"use strict";
+
+/* Get smal photo */
+let smallImg = document.querySelector("[src*='img/small_avatar.png']");
+let bigImg = document.querySelector("[src*='img/avatar.png']");
+smallImg.onclick = function(){
+  smallImg.classList.add('hidden');
+  bigImg.classList.remove('hidden');
+  span.style.display = 'block';
+}
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('bigPhoto__close')[0];
+
+// When the user clicks on <span> (x), close the modal
+function closePhoto() { 
+  span.style.display = 'none';
+  bigImg.classList.add('hidden');
+  smallImg.classList.remove('hidden');
+}
+
+span.onclick = closePhoto;
+bigImg.onclick = closePhoto;
+
