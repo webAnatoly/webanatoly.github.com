b2633df40d5822bbf1aa5c8a39eb7a3471a333a4
Tolik Dem <tolik.demianov@gmail.com>
2018-05-14 17:19:50 +0300

    добавил шаблон для быстрого старта разработки реактовских приложений

---
+++ myReactTemplate/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return(
+      <div>
+        Welcome to react!
+      </div>
+    )
+  }
+}
---
+++ myReactTemplate/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    filename: 'browser-bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      { 
+        test: /\.css$/, 
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: false,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
