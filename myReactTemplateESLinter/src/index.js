c30fda6b273ce239d1a2ccafa19b38107505a3f9
Tolik Dem <tolik.demianov@gmail.com>
2018-05-21 11:43:01 +0300

    some changes in template

--- myReactTemplateESLinter/src/index.js
+++ myReactTemplateESLinter/src/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './normalize.css';
-import App from './components/App';
+import App from './containers/App';
 
 ReactDOM.render(
   <App />,
